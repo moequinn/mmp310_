6eb5d86c643f8bcec79ad23334be5cf5c82be220
Moe Quinn <moequinn50@gmail.com>
2019-04-14 19:47:00 -0400

    add

--- spaceship_object_1/asteroid.js
+++ spaceship_object_1/asteroid.js
@@ -1,5 +1,6 @@
 /*  Asteroid class  */
 
+
 class Asteroid extends Entity {
 	constructor() {
 		super(random(width), -100);
@@ -7,13 +8,14 @@ class Asteroid extends Entity {
 		this.speed.y = random(5);
 		
 		this.color = color(random(100, 200), random(200), random(200));
+        this.offset = (random(width/2) / 2);
 	}
 	
 	display() {
 		fill(this.color);
 		noStroke();
-//		ellipse(this.x, this.y, this.size);
-        image(asteroidImage, this.x - 120, this.y - 100, 200, 200); // asteroid Image
+//		ellipse(this.x, this.y, this.size + this.offset); // testing position
+        image(asteroidImage, this.x - 120, this.y - 100,  this.size + this.offset, this.size + this.offset); // asteroid Image
 	}
 }
 
